e0f6117189770f44b08094b1e38bf5ea61263670
Arseniy Turikov <150505627+ArsushkaVolt@users.noreply.github.com>
2024-10-22 12:14:48 +0300

    d.z 22.10

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Robot robot1 = new RobotA();
+        Robot robot2 = new RobotB();
+
+        robot1.sayHello();
+        robot2.sayHello();
+    }
+}
